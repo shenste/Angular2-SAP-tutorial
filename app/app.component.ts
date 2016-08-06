d36f7c6bc662b27ef10cacd4b99df623dd176f19
sshen <sshen@central1.com>
2016-08-06 13:17:40 -0700

    Angular 2 final SPA initial commit with nav bar

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+import {NavBarComponent} from "./navbar-component";
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <nav-bar></nav-bar>
+    `,
+    directives: [NavBarComponent]
+})
+export class AppComponent { }
---
+++ app/navbar-component.ts
@@ -0,0 +1,17 @@
+import {Component} from "angular2/core";
+
+@Component({
+    selector: 'nav-bar',
+    templateUrl: 'app/template/navbar.component.html',
+    styles: [`
+    
+    `],
+    directives: [],
+    providers: []
+    
+
+})
+
+export class NavBarComponent {
+    
+}
