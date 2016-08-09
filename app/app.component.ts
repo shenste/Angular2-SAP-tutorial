54aaf41ddea8c5edfee1579eb3648fc8fefd88e3
sshen <sshen@central1.com>
2016-08-09 13:33:46 -0700

    Build a form to add a user.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,13 @@ import {RouteConfig, ROUTER_DIRECTIVES} from "angular2/router";
 import {UsersComponent} from "./users.component";
 import {PostsComponent} from "./posts.component";
 import {HomeComponent} from "./home.component";
+import {UserFormComponent} from "./user-form.component";
 
 
 @RouteConfig([
     { path: '/', name: 'Home', component: HomeComponent },
     { path: '/users', name: 'Users', component: UsersComponent },
+    { path: '/users/new', name: 'NewUser', component: UserFormComponent },
     { path: '/posts', name: 'Posts', component: PostsComponent },
     { path: '/*other', name: 'Other', redirectTo:['Home'] }
 ])
--- app/users.component.ts
+++ app/users.component.ts
@@ -2,31 +2,13 @@ import {Component, OnInit} from "angular2/core";
 import {UserService} from "./user.service";
 import {Observable} from "rxjs/Observable";
 import 'rxjs/add/operator/map';
+import {RouterLink} from "angular2/router";
 
 @Component({
     selector: 'user',
-    template: `
-        <h1>Users</h1>
-        <table class="table table-bordered">
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
-            <tr *ngFor="#user of users">
-                <th>{{user.name}}</th>
-                <th>{{user.email}}</th>
-                <th><i class="glyphicon glyphicon-edit"></i></th>
-                <th><i class="glyphicon glyphicon-remove"></i></th>
-            </tr>
-            
-            
-        
-        </table>
-        
-    `,
-    providers: [UserService]
+    templateUrl: 'app/template/users.component.html',
+    providers: [UserService],
+    directives: [RouterLink]
 })
 
 export class UsersComponent implements OnInit {
