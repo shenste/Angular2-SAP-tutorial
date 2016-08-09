8fabdc46efddd9d3352fded364d9b2097ba2c38e
sshen <sshen@central1.com>
2016-08-09 14:40:03 -0700

    dirty checking

--- app/user-form.component.ts
+++ app/user-form.component.ts
@@ -1,11 +1,12 @@
 import {Component} from 'angular2/core';
 import {FormBuilder, ControlGroup, Validator, Validators} from 'angular2/common';
 import {BasicValidators} from "./validator/basicValidators";
+import {CanDeactivate} from "angular2/router";
 
 @Component({
     templateUrl: 'app/template/user-form.component.html'
 })
-export class UserFormComponent {
+export class UserFormComponent implements CanDeactivate{
     form: ControlGroup;
 
     constructor(fb: FormBuilder) {
@@ -22,5 +23,12 @@ export class UserFormComponent {
                 zipcode: []
             })
         });
+    }
+
+    routerCanDeactivate(next, previous) {
+        if (this.form.dirty) {
+            return confirm('You have unsaved changes. Are you sure you want to navigate away?');
+        }
+        return true;
     }
 }
