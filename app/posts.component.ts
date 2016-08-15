a143b9587a87b88656c6138b4b41443dfadd4e30
sshen <sshen@central1.com>
2016-08-14 17:01:37 -0700

    Implement master/detail to view post details.

--- app/posts.component.ts
+++ app/posts.component.ts
@@ -1,10 +1,27 @@
-import {Component, OnInit} from "angular2/core";
+import {Component, OnInit, Output, EventEmitter} from "angular2/core";
 import {PostService} from "./post.service";
 import {SpinnerComponent} from "./spinner.component";
 
 @Component({
     selector: 'post',
     templateUrl: 'app/template/posts.component.html',
+    styles: [`
+        .posts li { 
+            cursor: pointer; 
+        }
+         
+        .posts li:hover { 
+            background: #ecf0f1; 
+         }
+         
+        .list-group-item.active,   
+        .list-group-item.active:hover,   
+        .list-group-item.active:focus {  
+            background-color: #ecf0f1;  
+            border-color: #ecf0f1;   
+            color: #2c3e50; 
+        }
+    `],
     directives: [SpinnerComponent],
     providers: [PostService]
 
@@ -13,6 +30,8 @@ import {SpinnerComponent} from "./spinner.component";
 export class PostsComponent implements OnInit{
     posts = [];
     isLoading = true;
+    @Output() change = new EventEmitter();
+    currentPost = null;
     
     constructor(private _postService: PostService) {
         
@@ -27,4 +46,10 @@ export class PostsComponent implements OnInit{
                 }
             );
     }
+
+    onPostSelected(selectedPost) {
+        //this.currentPost = { title: selectedPost.title, body: selectedPost.body}
+        this.currentPost = selectedPost;
+    }
+    
 }
